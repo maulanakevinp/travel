094d26e52e92c1f63b942c3be4e43281b9f3b804
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2020-06-26 22:31:43 +0700

    update smartSpeed

--- resources/views/tour/show.blade.php
+++ resources/views/tour/show.blade.php
@@ -170,6 +170,7 @@
                 autoplay: true,
                 autoplayTimeout: 3000,
                 autoplayHoverPause: true,
+                smartSpeed:1000,
                 responsive: {
                     0: {
                         items: 1
